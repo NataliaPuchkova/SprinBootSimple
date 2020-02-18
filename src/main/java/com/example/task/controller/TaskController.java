a188f9f6633e885197c850c161bfa9ddde4c5a04
alfalavista <natalia.puchkova@gmail.com>
2020-02-17 18:41:57 -0600

    Added swagger

--- src/main/java/com/example/task/controller/TaskController.java
+++ src/main/java/com/example/task/controller/TaskController.java
@@ -2,6 +2,10 @@ package com.example.task.controller;
 
 import com.example.task.model.Task;
 import com.example.task.service.TaskService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Api(value = "Task Controller", description = "Operation with tasks")
 public class TaskController {
 
     @Autowired
@@ -18,7 +23,13 @@ public class TaskController {
     public String getHello(){
         return "Hello world!";
     }
-
+    @ApiOperation(value = "Returns all list of users.", response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @GetMapping(value = "/all")
     public List<Task> getAllTask(){
         return service.findAll();
